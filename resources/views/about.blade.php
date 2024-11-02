e98f1912af467b8a6ae027371014c248c4f22e4d
DaffaRiyadi03 <daffariyadi100@gmail.com>
2024-11-02 12:34:34 +0700

    Belajar Laravel 11 | 3. Struktur Folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman About</h1>
+    <h3>Owner{{ $nama }}</h3>
+    <img src="img/Planet9_Wallpaper_5000x2813 - Copy.jpg" alt="">
+</body>
+
+</html>
