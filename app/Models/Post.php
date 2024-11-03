28e9bc51e1c74425ca86c968a8dd4ac15e9ad034
DaffaRiyadi03 <daffariyadi100@gmail.com>
2024-11-03 17:13:00 +0700

    Belajar Laravel 11 | 10. Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use \Illuminate\Database\Eloquent\Factories\HasFactory;
 class Post extends Model
 {
     protected $fillable = ['tittle', 'author', 'slug', 'body'];
